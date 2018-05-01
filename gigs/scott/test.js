1d2d01a9e548d61e5a7eb1b484e2af3eed3acbf8
Miya Neha <miyaneha@gmail.com>
2018-05-02 00:47:22 +0530

    Update test.js

--- gigs/scott/test.js
+++ gigs/scott/test.js
@@ -1,1 +1,9 @@
 alert("test!!");
+
+var mh = document.getElementById("mainHeading");
+
+var newh = document.createElement("h2");
+var htext = document.createTextNode("Dynamically Added!");
+newh.appendChild(htext);
+
+mh.appendChild(newh);
